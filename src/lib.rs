bb8f595f1d666be7162f759731b96caf940827f1
Robin Freyler <robin.freyler@gmail.com>
2023-04-01 00:07:41 +0200

    define library skeleton API

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,57 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub use enum_ref::{EnumMut, EnumRef};
+pub use enum_tag::EnumTag;
+use std::marker::PhantomData;
+
+/// Implemented by Rust `enum` types that allow for space-efficient encoding.
+pub trait EnumCodec: EnumRef + EnumMut {
+    /// Generated type used to encode instances of `enum` type `Self`.
+    type Encoder: Encode<Item = Self> + Decode;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Trait implemented by generated `enum` encoders to encode items.
+pub trait Encode {
+    /// The key type used to uniquely identify encoded items.
+    type Key;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    /// The type of the `enum` that the [`Encoder`] can encode.
+    ///
+    /// This way the [`Encoder`] can derive the `enum` tag type and others.
+    type Item;
+
+    /// Encodes the `enum` value for the [`Encoder`] and returns a reference to it.
+    fn encode(&mut self, value: &Self::Item) -> Key<Self, Self::Item>;
+}
+
+/// Trait implemented by generated `enum` encoders to decode items.
+pub trait Decode: Encode
+where
+    <Self as Encode>::Item: EnumRef + EnumMut,
+{
+    /// Decode the `enum` value encoded in the [`Encoder`] and returns a copy of it.
+    fn decode(&self, eref: Self::Key) -> Option<<Self::Item as EnumRef>::Ref<'_>>;
+
+    /// Decode the `enum` value encoded in the [`Encoder`] and returns a copy of it.
+    fn decode_mut(&mut self, eref: Self::Key) -> Option<<Self::Item as EnumMut>::Mut<'_>>;
+}
+
+/// Trait implemented by generated `enum` encoders to efficiently decode and visit items.
+pub trait DecodeVisit<V: Visitor>: Encode {
+    /// Decode the `enum` value encoded in the [`Encoder`] and returns a copy of it.
+    fn decode_visit(&self, key: Self::Key, visitor: &mut V) -> <V as Visitor>::Output;
+}
+
+/// Trait implemented by decode visitors.
+pub trait Visitor {
+    type Output;
+}
+
+/// A reference to an encoded `Item` in an `Encoder`.
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Key<Encoder: ?Sized, Item: ?Sized> {
+    /// The index at which the encoded `Item` resides in the `Encoder`.
+    index: usize,
+    /// Marker to trick the Rust compiler that `Encoder` is in use.
+    marker_encoder: PhantomData<fn() -> Encoder>,
+    /// Marker to trick the Rust compiler that `Item` is in use.
+    marker_item: PhantomData<fn() -> Item>,
 }
